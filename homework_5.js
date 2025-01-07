adaf5c7f21dbf0574edf157ab33de9a9bf141a1a
evgeniylisenko <lisenko.ocdev@gmail.com>
2025-01-07 21:49:03 +0200

    fix

--- homework_5.js
+++ homework_5.js
@@ -1,37 +1,37 @@
 //Вам необхідно написати функцію reverseString(str), яка приймає на вхід рядок і повертає його у зворотному порядку.
 function reverseString(str) {
-    let result = ''
-
-    for (let i = str.length - 1; i >= 0; i--) {
-        result += str[i];
-    }
-    // return str.split("").reverse().join("");
-    return result;
+    // let result = ''
+    //
+    // for (let i = str.length - 1; i >= 0; i--) {
+    //     result += str[i];
+    // }
+    //
+    // return result;
+    return str.split("").reverse().join("");
 }
 
-console.log(reverseString('hellow world'));
+console.log(reverseString('hello world'));
 
 //Вам необхідно написати функцію isPalindrome(str), яка приймає на вхід рядок і перевіряє, чи є введений рядок паліндромом.
-const number = prompt("Введите значение");
+const string = prompt("Введите значение");
 
-function isPalindrome(number) {
-    const stringArr = reverseString(number);
-    const result = number == stringArr;
+function isPalindrome(str) {
+    const stringArr = reverseString(str);
 
-    return result ? console.log('Це значення є паліндромом') : console.log('Це значення не є паліндромом');
+    return str == stringArr ? 'Це значення є паліндромом' : 'Це значення не є паліндромом';
 }
 
-number ? console.log(isPalindrome(number)) : '';
+string ? console.log(isPalindrome(string)) : '';
 
 //Вам необхідно написати функцію findGCD(a, b), яка приймає на вхід два числа і повертає їхній НСД.
 function findGCD(a, b) {
-    while (a != 0 & b != 0) {
-
-        const sum = a > b;
-        const equals = b = b % a
-
-        return sum ? sum : equals;
+    while (b !== 0) {
+        const temp = b; // 55 20 15 5
+        b = a % b;      // 20 15 5 0 - після цього в цикл не заходить
+        a = temp;       // 55 20 15 5
     }
+
+    return a;
 }
 
 console.log(findGCD(20, 55));
